9918fd61fdcd9f53a08b28e04ec36723f1b3dfe8
Miguel Hernanz <migherdcas@gmail.com>
2020-09-28 16:52:47 +0200

    feat(Student): Student properties and methods (WIP).

--- src/Student.ts
+++ src/Student.ts
@@ -1,25 +1,47 @@
-import { Name } from "shared/sharedModels";
+import { StudentName } from "shared/sharedModels";
+import Grade from "./Grade";
 
-interface StudentProps {
-  currentlyCoursing: Course | null;
-  coursedDegrees: Course[];
+export interface StudentProps {
+  currentlyCoursing: Grade | null;
+  coursedDegrees: Grade[];
 }
 
 export default class Student {
-  private readonly name: Name;
-  private bornYear: number;
-
+  private readonly studentName: StudentName;
+  private readonly bornYear: number;
   private readonly staticProps: StudentProps;
 
-  constructor(name: Name, bornYear: number, staticProps: StudentProps) {
-    this.name = name;
+  constructor(
+    studentName: StudentName,
+    bornYear: number,
+    staticProps: StudentProps
+  ) {
+    this.studentName = studentName;
     this.bornYear = bornYear;
     this.staticProps = staticProps;
   }
 
+  public getName(): string {
+    return this.studentName;
+  }
+
   public getAge(): number {
     const newDate = new Date();
-
     return newDate.getFullYear() - this.bornYear;
+  }
+
+  public getProps() {
+    return this.staticProps;
+  }
+
+  public completeGrade(): void {
+    let { coursedDegrees, currentlyCoursing } = this.staticProps;
+    coursedDegrees.push(currentlyCoursing!);
+    currentlyCoursing = null;
+  }
+
+  // Quit current grade
+  private static quitGrade(student: Student): void {
+    student.staticProps.currentlyCoursing = null;
   }
 }
