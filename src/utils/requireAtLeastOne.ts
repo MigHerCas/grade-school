93bf7dcf403d92c5f2073598c4dd3b6d1960d4ee
Miguel Hernanz <migherdcas@gmail.com>
2020-09-28 16:52:08 +0200

    feat(Utils): Adds some helping functions.

---
+++ src/utils/requireAtLeastOne.ts
@@ -0,0 +1,10 @@
+// Typescript interface requires one of two properties to exist
+// https://stackoverflow.com/a/49725198/7387747
+export type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<
+  T,
+  Exclude<keyof T, Keys>
+> &
+  {
+    [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>;
+  }[Keys];
+
